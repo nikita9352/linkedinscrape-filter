b5bc021e8672d0444817a04a15300a26efb0920e
Emre <basarremre@outlook.com>
2022-05-14 03:29:48 +0300

    hiring status and search button is fixed

--- streamlitlinkedin.py
+++ streamlitlinkedin.py
@@ -86,8 +86,8 @@ columnss = ['position name',
         'ad language',
         'job link',
         'easy apply']
-but = st.button('Search Jobs', key='button_add_project',
-          on_click=linkedin_complete, args=[email,password])
+but = st.checkbox('Search Jobs',help='To see result check the box')
+st.write('if Search Jobs checked you will see the table otherways it will be blank table!')
 if but:
     dataframe = linkedin_complete(email,password)
 else:
@@ -134,12 +134,17 @@ max = z['number of applicants'].max() if z['number of applicants'].max() >0 else
 Number_of_applications = st.sidebar.slider('Applicants ', help='setted number represenents smaller number of applicants',min_value=int(z['number of applicants'].min()), max_value=int(max), value = int(z['number of applicants'].max()))
 selected_zzz = languge_workplace_selected_z[languge_workplace_selected_z['number of applicants'] <= Number_of_applications]
 
+try:
+    hiring_s = selected_zzz['hiring status'].unique()
+except:
+    hiring_s = []
 
 
-
-hiring = st.selectbox('Hiring Status',selected_zzz['hiring status'].unique())
+hiring = st.sidebar.multiselect('Hiring Status',hiring_s,hiring_s)
 # hiring = str(hiring)
-selected_zzzz = selected_zzz[selected_zzz['hiring status'] == hiring]
+
+
+selected_zzzz = selected_zzz[selected_zzz['hiring status'].isin(hiring)]
 
 easy_apply = str(st.checkbox('Easy Apply',value=False,help='Check the box and click the nested button to apply the job'))
 # activate the easy apply button if the checkbox is checked and easy apply button provide to apply the job
@@ -153,7 +158,8 @@ selected_zzzzz = selected_zzzz[selected_zzzz['easy apply'] == easy_apply]
 # st.write(selected_zzzzz.to_html(escape=False, index=False), unsafe_allow_html=True)
 
 
-st.header('Job Search Results')
+st.header('Job Search Results',)
+
 
 # Grid options
 
